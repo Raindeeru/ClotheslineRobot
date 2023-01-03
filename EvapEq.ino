5acdf32ab6b5935e79df6ff82c14494236a880a3
Raindeer <derv.gonzales17@gmail.com>
2023-01-03 18:29:33 +0800

    first commit

---
+++ EvapEq.ino
@@ -0,0 +1,15 @@
+void setup()
+{
+    
+}
+
+void loop()
+{
+    
+}
+
+//This function measures the evaporation rate using penmann-montieth equation
+float EvapRate(float humidity, float temp, float irradiance, float windSpeed)
+{
+
+}
