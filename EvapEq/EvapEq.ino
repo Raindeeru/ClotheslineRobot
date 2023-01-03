eac483afde4666fb0d05f679523412da1028c0ed
Raindeer <derv.gonzales17@gmail.com>
2023-01-03 20:20:36 +0800

    another commit

--- EvapEq/EvapEq.ino
+++ EvapEq/EvapEq.ino
@@ -1,15 +1,23 @@
+int x = 0;
+
 void setup()
 {
-    
+    Serial.begin(9600);
 }
 
 void loop()
 {
-    
+    Serial.println(exp(x));
+    if(x<10)
+    {
+        x++;
+    }
 }
 
 //This function measures the evaporation rate using penmann-montieth equation
 float EvapRate(float humidity, float temp, float irradiance, float windSpeed)
 {
-
+    float e;
+    float delta;
+    float x = exp(2);
 }
