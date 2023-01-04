e799f6218f77134a2c209fb613b55d979d5fb05b
Raindeer <derv.gonzales17@gmail.com>
2023-01-04 16:04:59 +0800

    added equation and threshold functions

--- Sensors/Sensors.ino
+++ Sensors/Sensors.ino
@@ -1,6 +1,8 @@
 #include <DHT.h>
 
-unsigned long timemeasure = 1.00; // seconds
+unsigned long timemeasure = 10.00; // seconds
+
+float threshold = 25.0;
 
 // anemometer parameters
 volatile byte rpmcount; // count signals
@@ -10,7 +12,7 @@ int timetoSleep = 1;               // minutes
 unsigned long sleepTime = 15;      // minutes
 unsigned long timeNow;
 int countThing = 0;
-int GPIO_pulse = 4; // Arduino = D2
+int GPIO_pulse = 2; // Arduino = D2
 float rpm, rps;     // frequencies
 float radius = 0.1; // meters - measure of the lenght of each the anemometer wing
 float velocity_kmh; // km/h
@@ -19,7 +21,7 @@ float omega = 0;    // rad/s
 float calibration_value = 2.0;
 
 //dht22 parameters
-#define DHTpin 2
+#define DHTpin 4
 #define DHTtype DHT22
 float temperature;
 float humidity;
@@ -33,8 +35,9 @@ void setup()
 {
     dht.begin();
     
-    pinMode(4, INPUT);
     pinMode(A0, INPUT);
+    pinMode(4, INPUT);
+    pinMode(13,OUTPUT);
 
     pinMode(GPIO_pulse, INPUT_PULLUP);
     digitalWrite(GPIO_pulse, LOW);
@@ -66,6 +69,8 @@ void loop()
     omega = 2 * PI * rps;                               // rad/s
     velocity_ms = omega * radius * calibration_value;   // m/s
     velocity_kmh = velocity_ms * 3.6;                   // km/h
+    
+    Serial.println("________________________________");
 
     Serial.print("Wind Speed: ");
     Serial.print(velocity_ms);
@@ -82,6 +87,23 @@ void loop()
     Serial.print("Solar Radiation: ");
     Serial.print(irradiance/50);
     Serial.println("MW/m^2");
+
+    float decHum = humidity * 0.01;
+    Serial.println(decHum);
+    float evapRate = EvapRate(decHum, temperature+273, irradiance, velocity_ms);
+
+    Serial.print("Evaporation Rate: ");
+    Serial.println(evapRate);
+
+    if(evapRate>threshold)
+    {
+        digitalWrite(13, 1);
+        Serial.println("yo yo yo!");
+    }
+    else
+    {
+        digitalWrite(13, 0);
+    }
 
     if (countThing == 1) // Send data per timeasure seconds
     {
@@ -102,4 +124,17 @@ void rpm_anemometer()
     last_micros = micros();
   }
   //   Serial.println("***** detect *****");
+}
+
+float EvapRate(float hum, float temp, float rad, float windSpeed)
+{
+    float evapRate;
+    float lambda(2.501 - 0.002361*(temp - 273));
+    float y = 165.02/lambda;
+    float ea = exp(21.07 - (5336/temp));
+    float D = (1-hum)*ea;
+    float delta = (5336/pow(temp,2))*D;
+
+    evapRate = (delta*rad + y*(6.43*(1+0.536*windSpeed)*D))/(lambda*(delta+y));
+    return evapRate;
 }
