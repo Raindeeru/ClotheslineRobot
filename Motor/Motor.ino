96cecccb0bf4814c4b4b0da0ac6d4b93784d44dd
Raindeer <derv.gonzales17@gmail.com>
2023-01-04 10:34:41 +0800

    Added comments if ever gusto niyo itry magcode

--- Motor/Motor.ino
+++ Motor/Motor.ino
@@ -1,11 +1,11 @@
 // Motor A connections
-int enA = 9;
-int in1 = 8;
-int in2 = 7;
+int enA = 9; //Speed Control A;
+int in1 = 8; //Turn this on, the motor spins forward
+int in2 = 7; //Turn this on, the motor spins backward
 // Motor B connections
-int enB = 3;
-int in3 = 5;
-int in4 = 4;
+int enB = 3; //Speed Control B
+int in3 = 5; //Turn this on, the motor spins forward
+int in4 = 4; //Turn this on the motor spins backward
 
 void setup()
 {
