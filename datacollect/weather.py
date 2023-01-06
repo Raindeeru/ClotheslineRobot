136145b5c3c6092fb0a87cb8cd88fe528dc7ccac
Raindeer <derv.gonzales17@gmail.com>
2023-01-06 23:49:47 +0800

    added the equation in

--- datacollect/weather.py
+++ datacollect/weather.py
@@ -10,6 +10,7 @@ latitude = '12.9927'
 longitude = '124.0147'
 
 solurl = 'https://api.solcast.com.au/world_radiation/estimated_actuals?latitude='+latitude+'&longitude='+longitude+'&hours=48&format=json&api_key='+SOLCAST_KEY
+
 owmreq = requests.get('https://api.openweathermap.org/data/2.5/weather?id=1685759&appid='+ OPENWEATHERMAP_KEY)
 owmdata = owmreq.json()
 
@@ -21,8 +22,6 @@ headers = {
 screq = requests.request("GET", solurl, headers=headers, data=payload)
 soldata = screq.json()
 
-print(json.dumps(owmdata, indent=4))
-
 temp = owmdata['main']['temp']+273.15
 hum = owmdata['main']['humidity']
 wind = owmdata['wind']['speed']
@@ -33,3 +32,22 @@ print('Humidity = '+str(hum)+'%')
 print('Wind Speed = '+str(wind)+'m/s')
 print('Irradiance = '+str(rad)+'W/m^2')
 
+def EvapEq(h,t,u,r):
+    lv = (2501000) - (2370*t)
+    print(lv)
+    Er = (r)/(lv*997)*86400000
+    print(Er)
+
+    b = (0.102*u)/77.53;
+    eas = 611*math.exp((17.27*t)/(237.3+t))
+    ea = h*eas
+    Ea = b*(eas-ea)
+
+    d = (4098*eas)/math.pow(237.3+t,2);
+    print(d)
+    e = ((d/(d+66.8))*Er)+((66.8/(d+66.8))*Ea)
+
+    return e
+
+evaprate = EvapEq(hum*0.01,temp+273.15,wind,rad)
+print('Evaporation Rate = '+str(evaprate)+'mm/day')
