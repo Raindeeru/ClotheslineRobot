cdcdfbc581471b80f4e387d306c5186f765147e5
Raindeer <derv.gonzales17@gmail.com>
2023-01-07 18:41:16 +0800

    converted py file into executable, can be found in dist

--- datacollect/weather.py
+++ datacollect/weather.py
@@ -9,6 +9,11 @@ OPENWEATHERMAP_KEY = '69daeb9d1aaca0b933d930f4e185e3ea'
 latitude = '12.9927'
 longitude = '124.0147'
 
+threshold = 6
+
+print('hi')
+input()
+
 solurl = 'https://api.solcast.com.au/world_radiation/estimated_actuals?latitude='+latitude+'&longitude='+longitude+'&hours=48&format=json&api_key='+SOLCAST_KEY
 
 owmreq = requests.get('https://api.openweathermap.org/data/2.5/weather?id=1685759&appid='+ OPENWEATHERMAP_KEY)
@@ -19,24 +24,23 @@ headers = {
   'Accept': 'application/json'
 }
 
-screq = requests.request("GET", solurl, headers=headers, data=payload)
-soldata = screq.json()
+#screq = requests.request("GET", solurl, headers=headers, data=payload)
+#soldata = screq.json()
 
-temp = owmdata['main']['temp']+273.15
+temp = owmdata['main']['temp']-273.15
 hum = owmdata['main']['humidity']
 wind = owmdata['wind']['speed']
-rad = soldata['estimated_actuals'][0]['ghi']
+#rad = soldata['estimated_actuals'][0]['ghi']
+
 
 print('Temperature = '+str(temp)+'°C')
 print('Humidity = '+str(hum)+'%')
 print('Wind Speed = '+str(wind)+'m/s')
-print('Irradiance = '+str(rad)+'W/m^2')
+#print('Irradiance = '+str(rad)+'W/m^2')
 
 def EvapEq(h,t,u,r):
     lv = (2501000) - (2370*t)
-    print(lv)
     Er = (r)/(lv*997)*86400000
-    print(Er)
 
     b = (0.102*u)/77.53;
     eas = 611*math.exp((17.27*t)/(237.3+t))
@@ -44,10 +48,14 @@ def EvapEq(h,t,u,r):
     Ea = b*(eas-ea)
 
     d = (4098*eas)/math.pow(237.3+t,2);
-    print(d)
     e = ((d/(d+66.8))*Er)+((66.8/(d+66.8))*Ea)
 
     return e
 
-evaprate = EvapEq(hum*0.01,temp+273.15,wind,rad)
+evaprate = EvapEq(hum*0.01,temp,wind,190)
 print('Evaporation Rate = '+str(evaprate)+'mm/day')
+
+if evaprate>threshold:
+  print('Clothes Outside')
+else:
+  print('Clothes Inside')
